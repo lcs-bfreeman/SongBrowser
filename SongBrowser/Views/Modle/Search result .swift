528dd53850df916b93ea9057f12e7b9487c42297
Benjamn <bfreeman@lcs.on.ca>
2021-02-20 10:15:47 -0500

    Defined the model for the parts of the JSON file we need to retreate

---
+++ SongBrowser/Views/Modle/Search result .swift
@@ -0,0 +1,12 @@
+//
+//  Search result .swift
+//  SongBrowser
+//
+//  Created by Ben Freeman on 2021-02-20.
+//
+
+import Foundation
+
+struct SearchResult: Decodable {
+    let results: [Song]
+}
